479507acf8dd8ab46357a75a730105d4c96e9c24
Nicholas Grasevski <nicholas@localhost.localdomain>
2014-01-06 23:19:58 +1100

    Real estate gui complete

--- tkrealestate.py
+++ tkrealestate.py
@@ -9,19 +9,15 @@ import realestate
 def main():
     """Load the gui"""
     top = Tkinter.Tk()
-    frame = ttk.Frame(top, relief=Tkinter.RIDGE, borderwidth=2)
-    frame.pack()
+    frame = ttk.Frame(top)
+    frame.grid()
     command = Tkinter.StringVar(top, 'buy')
-    options = 'buy', 'rent', 'sold'
-    group = ttk.LabelFrame(frame, text='Listing types')
+    group = ttk.LabelFrame(frame, text='Listing type')
     group.grid(row=0, column=0)
-    for index in range(len(options)):
-        opt = options[index]
-        btn = ttk.Radiobutton(group, text=opt, variable=command, value=opt)
-        btn.pack()
-    ttk.Label(frame, text='Search:').grid(row=0, column=1)
+    for opt in 'buy', 'rent', 'sold':
+        ttk.Radiobutton(group, text=opt, variable=command, value=opt).grid()
     search = ttk.Entry(frame)
-    search.grid(row=0, column=2)
+    search.grid(row=0, column=1)
 
     def export():
         """Run the real estate program"""
@@ -31,8 +27,10 @@ def main():
             realestate.realestate(command.get(), search.get(), filehandle)
             filehandle.close()
 
-    button = ttk.Button(frame, text='Export', command=export)
-    button.grid(row=0, column=3)
+    button = ttk.Button(frame, text='Fetch', command=export)
+    button.grid(row=0, column=2)
+    for child in frame.winfo_children():
+        child.grid_configure(padx=5, pady=5)
     top.mainloop()
 
 if __name__ == '__main__':
